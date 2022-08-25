648ddb930f47d000d76c9564b599bef5056295cd
schurterb <brandon.schurter@protonmail.com>
2022-08-25 17:25:37 -0400

    Imported partially converted voxeljs-hello-world demo.

---
+++ www/js/domnode-dom.js
@@ -0,0 +1,4 @@
+function domnode_dom(require, module, exports) {
+    module.exports = require('./lib/index')
+
+}
---
+++ www/js/from.js
@@ -0,0 +1,6 @@
+function from(require, module, exports) {
+    module.exports = function() {
+        throw new Error('Readable.from is not available in the browser')
+    };
+
+}
