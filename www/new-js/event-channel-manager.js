f7373f36f761b5d4d4f7bcbcf4906f5b9d81998d
schurterb <brandon.schurter@protonmail.com>
2022-11-14 17:43:09 -0500

    Prototyped channelManager and event channels.

---
+++ www/new-js/event-channel-manager.js
@@ -0,0 +1,95 @@
+/*
+  Multi-directional event channels.
+  These channels allow events to be passed throughout the main thread, between
+  the main thread and worker threads, and between different browsers (via WebRTC)
+
+  EventChannel config ::
+  {
+    'local': <boolean>,
+    'workers': <boolean>,
+    'distributed': <boolean>,
+    'events': [...] //List of supported events
+  }
+
+  EventChannels only send.  To do worker-to-worker event routing, a worker must
+  have an onMessage handler set up to forward the message to the proper channels.
+
+*/
+
+const eventChannelManager = {
+
+  eventChannels: {},
+
+  listChannels: function() {
+    return Object.keys(this.eventChannels);
+  },
+
+  getChannel: function(id) {
+    return this.eventChannels[id];
+  },
+
+  addChannel: function(id, opts) {
+    if(!this.eventChannels[id]) {
+      this.eventChannels[id] = new EventChannel(id, opts);
+    }
+    return this.eventChannels[id];
+  },
+
+  removeChannel: function(id) {
+    if(this.eventChannels[id]) {
+      this.eventChannels[id].close();
+      delete this.eventChannels[id];
+    }
+  }
+};
+
+class EventChannel() {
+  constructor(id, opts) {
+    this.id = id;
+    this.events = opts.events; //required
+
+    // Enable local events
+    this.local = opts.local || false;
+
+    // Enable worker events
+    this.workers = opts.workers || false;
+    if(this.workers) {
+      this.workerList = [];
+    }
+
+    // Enable distributed events
+    this.distrubted = opts.distrubted || false;
+    if(this.distrubted) {
+      console.log("[EventChannel] TODO: Implement distributed events via WebRTC Data Channels");
+    }
+  }
+
+  subscribe(entity) {
+    if(typeof(entity) === 'Worker') {
+      this.workerList.push(entity);
+    }
+    //TODO: implement distributed events via WebRTC Data Channels
+  }
+  unsubscribe(entity) {
+    if(typeof(entity) === 'Worker') {
+      var idx = this.workerList.indexOf(entity);
+      if(idx > 0) { this.workerList = this.workerList.split(idx, 1); }
+    }
+    //TODO: implement distributed events via WebRTC Data Channels
+  }
+
+  //TODO: how to create proper event from CustomEvent
+  send(event) {
+    if(this.local) {
+      document.dispatchEvent(event);
+    }
+    if(this.workers) {
+      for(var i in this.workers) {
+        this.workers[i].postMessage(event);
+      }
+    }
+    if(this.distributed) {
+      console.log("[EventChannel] TODO: Implement distributed events via WebRTC Data Channels");
+    }
+  }
+}
--- www/test/test-module.js
+++ www/test/test-module.js
@@ -2,4 +2,9 @@
   Test module for verifying the various aspects of the module-loader work correctly.
 */
 
-  console.log(`[test-module] PASS`);
+//TODO: handle events
+
+//TODO: test sending messages
+
+
+console.log(`[test-module] LOADED`);
